52b3322f7f92280ce70fb7dfd495dff3c14544c3
Kaiser-Tanveer <kaisertanveer0@gmail.com>
2026-08-25 21:19:15 +0600

    product utils added

---
+++ src/features/products/ProductUtils.tsx
@@ -0,0 +1,40 @@
+import type { Product } from "./ProductTypes";
+
+export const calculateProfit = (
+  product: Product
+) => {
+  return (
+    product.sellingPrice -
+    product.purchasePrice
+  );
+};
+
+export const calculateProfitMargin = (
+  product: Product
+) => {
+  if (product.sellingPrice === 0) {
+    return 0;
+  }
+
+  return (
+    ((product.sellingPrice -
+      product.purchasePrice) /
+      product.sellingPrice) *
+    100
+  );
+};
+
+export const isLowStock = (
+  product: Product
+) => {
+  return (
+    product.stockQuantity <=
+    product.lowStockThreshold
+  );
+};
+
+export const isOutOfStock = (
+  product: Product
+) => {
+  return product.stockQuantity <= 0;
+};
